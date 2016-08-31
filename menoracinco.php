c987eb063b322a8e6308a6a34422242e65c251e4
MCosentino <muc_358@hotmail.com>
2016-08-30 21:17:28 -0300

    Se agrega estilo a todo

--- menoracinco.php
+++ menoracinco.php
@@ -1,10 +1,16 @@
 <html>
 <head>
 	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel="stylesheet" type="text/css" href="animacion.css">
+
 </head>
 <body>
+	<p>
 	<?php 
 	echo "El mensaje: ". $_POST['mensaje']. " tiene un largo menor o igual a 5";
 	 ?>
+	 </p>
 </body>
 </html>
